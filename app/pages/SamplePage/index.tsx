b6c4258f2b6b755db2438095a1dd9a5992b5d697
Avanthik M <avanthikm@Avanthiks-MacBook-Pro.local>
2025-04-01 17:08:37 +0530

    feat: add new sample page

---
+++ app/pages/SamplePage/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SamplePage = () => {
+  return (
+    <View>
+      <Text>Hello SamplePage</Text>
+    </View>
+  );
+};
+
+export default SamplePage;
